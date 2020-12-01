ac2ded3fa8a0ad4904b42fc584198e54392eac8d
Daniel Li <danielli12750@gmail.com>
2020-12-01 23:59:08 +1100

    removed a plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,18 +40,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/android-chrome-512x512.png`, // This path is relative to the root of the site.
-      },
-    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
